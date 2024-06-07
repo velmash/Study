e068ab85e95a387e188ea3406d12effd4406f9bd
윤형찬 <72960152+velmash@users.noreply.github.com>
2024-06-07 13:25:38 +0900

    :wrench: Reactor 구현

---
+++ ReactorKit/ExReactorKit/ExReactorKit/TextFieldViewReactor.swift
@@ -0,0 +1,56 @@
+//
+//  TextFieldViewReactor.swift
+//  ExReactorKit
+//
+//  Created by 윤형찬 on 6/7/24.
+//
+
+import UIKit
+import ReactorKit
+import RxSwift
+
+final class TextFieldViewReactor: Reactor {
+    enum Action {
+        case inputField(String)
+    }
+    
+    enum Mutation {
+        case setLengthOfString(Int)
+        case setCapitalizedString(String)
+    }
+    
+    struct State {
+        var capitalizedString: String?
+        var lengthOfString: Int?
+    }
+    
+    var initialState: State
+    
+    init() {
+        self.initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .inputField(input):
+            let lengthOfString = input.count
+            let capitalizedString = input.uppercased()
+            return Observable<Mutation>.concat(
+                Observable.just(.setLengthOfString(lengthOfString)),
+                Observable.just(.setCapitalizedString(capitalizedString))
+            )
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        switch mutation {
+        case let .setLengthOfString(length):
+            newState.lengthOfString = length
+        case let .setCapitalizedString(string):
+            newState.capitalizedString = string
+        }
+        
+        return newState
+    }
+}
